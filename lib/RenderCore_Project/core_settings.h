eadd05994392b73de2342bdada2dcf7f4d1af022
Christian Oliveros <christianol_01@hotmail.com>
2020-11-14 09:03:59 -0400

    More Ray stuff and the approximately func

--- lib/RenderCore_Project/core_settings.h
+++ lib/RenderCore_Project/core_settings.h
@@ -25,6 +25,7 @@
 using namespace lighthouse2;
 
 #include "core_api_base.h"
+#include "general_utils.h"
 #include "intersection_utils.h"
 #include "rendercore.h"
 
--- lib/RenderCore_Project/intersection_utils.h
+++ lib/RenderCore_Project/intersection_utils.h
@@ -25,26 +25,44 @@ public:
 	float4 origin;
 	float4 direction;
 
-	// No Copy
+	// No copy by accident
 	Ray(const Ray&) = delete;
 	Ray& operator=(const Ray&) = delete;
 
+	/// <summary>
+	/// Copy Ray
+	/// </summary>
+	/// <returns>New Copied Ray</returns>
+	inline Ray Copy()
+	{
+		return Ray(origin, direction);
+	}
+
 	inline Ray(const float4 &ori, const float4 &dir)
 	{
+		assert(almost_equal(1, length(dir)), "Unormalized Vector");
 		origin = ori;
 		direction = dir;
 	}
 
 	inline Ray(const float3& ori, const float3& dir)
 	{
+		assert(almost_equal(1, length(dir)), "Unormalized Vector");
 		origin = make_float4(ori);
 		direction = make_float4(dir);
 	}
 
+	/// <summary>
+	/// Evalute the ray for certain t
+	/// </summary>
+	/// <param name="t">Parameter</param>
+	/// <returns>Position</returns>
 	inline float4 Evaluate(const float t)
 	{
 		return origin + (t * direction);
 	}
+
+	
 };
 
 
