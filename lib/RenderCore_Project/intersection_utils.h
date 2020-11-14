06ba62e575fabe7a6e2545630996ae0338d44033
Kayleigh Schoorl <kayleighschoorl@hotmail.com>
2020-11-14 15:50:53 +0100

    Add empty constructor

--- lib/RenderCore_Project/intersection_utils.h
+++ lib/RenderCore_Project/intersection_utils.h
@@ -25,30 +25,118 @@ public:
 	float4 origin;
 	float4 direction;
 
-	// No Copy
+	// No copy by accident
 	Ray(const Ray&) = delete;
 	Ray& operator=(const Ray&) = delete;
 
-	inline Ray() {}
+	/// <summary>
+	/// Copy Ray
+	/// </summary>
+	/// <returns>New Copied Ray</returns>
+	inline Ray Copy()
+	{
+		return Ray(origin, direction);
+	}
 
 	inline Ray(const float4 &ori, const float4 &dir)
 	{
+		assert(almost_equal(1, length(dir))); // "Unormalized Vector"
 		origin = ori;
 		direction = dir;
 	}
 
 	inline Ray(const float3& ori, const float3& dir)
 	{
+		assert(almost_equal(1, length(dir))); // "Unormalized Vector"
 		origin = make_float4(ori);
 		direction = make_float4(dir);
 	}
 
+	inline void SetOrigin(const float4& ori)
+	{
+		origin = ori;
+	}
+	inline void SetOrigin(const float3& ori)
+	{
+		origin = make_float4(ori);
+	}
+
+	inline void SetDirection(const float4& dir)
+	{
+		direction = dir;
+	}
+	inline void SetDirection(const float3& dir)
+	{
+		direction = make_float4(dir);
+	}
+
+	/// <summary>
+	/// Evalute the ray for certain t
+	/// </summary>
+	/// <param name="t">Parameter</param>
+	/// <returns>Position</returns>
 	inline float4 Evaluate(const float t)
 	{
 		return origin + (t * direction);
 	}
+
+	
 };
 
+
+/// <summary>
+/// Ray Triangle Interception Information
+/// </summary>
+struct RayTriangleInterceptInfo
+{
+public:
+	/// <summary>
+	/// Ray t Value
+	/// </summary>
+	float t;
+
+	/// <summary>
+	/// Barycentric u coordinate
+	/// </summary>
+	float u;
+
+	/// <summary>
+	/// Barycentric v coordinate
+	/// </summary>
+	float v;
+
+	/// <summary>
+	/// If the interception was back facing
+	/// </summary>
+	bool backFacing;
+
+	inline RayTriangleInterceptInfo()
+	{
+		t = 0;
+		u = 0;
+		v = 0;
+		backFacing = false;
+	}
+
+
+	inline float GetWCoord()
+	{
+		return 1 - u - v;
+	}
+
+	// No copy by accident
+	RayTriangleInterceptInfo(const RayTriangleInterceptInfo&) = delete;
+	RayTriangleInterceptInfo& operator=(const RayTriangleInterceptInfo&) = delete;
+
+	inline void CopyTo(RayTriangleInterceptInfo& info)
+	{
+		info.t = t;
+		info.u = u;
+		info.v = v;
+		info.backFacing = backFacing;
+	}
+
+};
 
 
 }
